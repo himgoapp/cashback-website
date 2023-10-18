593dceca3b94c62f4c430e0e245765fc659f990d
tahid02 <tanzim1463@gmail.com>
2023-10-18 12:28:18 +0600

    description page linked

--- src/components/offerAndDeal/cards/cardContainer.jsx
+++ src/components/offerAndDeal/cards/cardContainer.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import styles from "./cardContainer.module.css";
 import OfferCard from "./card";
+import { Link } from "react-router-dom";
 const OfferCardContainer = () => {
   return (
     <div className={styles.offer_cards_container}>
-      <OfferCard />
-      <OfferCard />
-      <OfferCard />
+      <Link to="/description" style={{ textDecoration: "none" }}>
+        <OfferCard />
+      </Link>
+      <Link to="/description" style={{ textDecoration: "none" }}>
+        <OfferCard />
+      </Link>
+      <Link to="/description" style={{ textDecoration: "none" }}>
+        <OfferCard />
+      </Link>
+
       <OfferCard />
       <OfferCard />
       <OfferCard />
