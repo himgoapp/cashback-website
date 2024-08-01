aef8230652f21d22c9768d9e5608d5b2937d5ed8
lalit rana <lalit.1995.rana@gmail.com>
2024-08-02 03:05:29 +0530

    changes for the config file

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const baseUrlconfig = {
-  // baseUrl: "http://82.112.234.40:8080/api",
-  baseUrl: "http://localhost:8080/api",
+  baseUrl: "http://82.112.234.40:8080/api",
+  // baseUrl: "http://localhost:8080/api",
 };
