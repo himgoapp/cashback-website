dda884810cacdfdb48695adb6b9c456bf7b7a159
lalit rana <lalit.1995.rana@gmail.com>
2024-08-01 16:32:59 +0530

    changes in the features branch

--- src/components/dashboard/sidebar/sidebar.jsx
+++ src/components/dashboard/sidebar/sidebar.jsx
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
 import styles from "./sidebar.module.css";
 import { Link } from "react-router-dom";
+import { UserContext } from "../../../App";
 
-const Sidebar = () => {
-  let data = localStorage.getItem("userInfo")
-    ? JSON.parse(localStorage.getItem("userInfo"))
+const Sidebar = ({ active }) => {
+  const { mobile } = useContext(UserContext);
+  let data = sessionStorage.getItem("allInfo")
+    ? JSON.parse(sessionStorage.getItem("allInfo"))
     : {};
-
+  // console.log({ showSidebar });
   return (
-    <div className={styles.SidebarNavigation}>
+    <div
+      className={styles.SidebarNavigation}
+      // style={!mobile ? { display: "inline-flex" } : { display: "none" }}
+      style={{ display: "inline-flex" }}
+    >
       <div className={styles.Content}>
         <div className={styles.Nav}>
           <div className={styles.Header}>
@@ -31,11 +37,15 @@ const Sidebar = () => {
                 text: "My Transactions",
                 link: "/dashboard/mytransactions",
               },
-            ].map((nav) => {
+            ].map((nav, index) => {
               return (
                 <Link
                   to={nav.link}
-                  style={{ textDecoration: "none", width: "100%" }}
+                  style={{
+                    textDecoration: "none",
+                    width: "100%",
+                    background: active == index && "#4072ff",
+                  }}
                   className={styles.NavItemBase}
                 >
                   <div className={styles.ItemContent}>
@@ -58,11 +68,11 @@ const Sidebar = () => {
                 alt="Avatar"
               />
               <div className={styles.TextAndSupportingText}>
-                <div className={styles.Text}>
-                  {data && data.userName ? data.userName : "Not filled!"}
-                </div>
+                {data && data.user.userName
+                  ? data.user.userName
+                  : "Not filled!"}
                 <div className={styles.SupportingText}>
-                  {data && data.email ? data.email : "Not filled!"}
+                  {data && data.user.email ? data.user.email : "Not filled!"}
                 </div>
               </div>
             </div>
@@ -73,12 +83,35 @@ const Sidebar = () => {
             </div>
           </div>
         </div>
+        {/* <div
+          className={styles.CloseSidebar}
+          onClick={() => setShowSidebar(false)}
+        >
+          {closeIcon}
+        </div> */}
       </div>
     </div>
   );
 };
 
 export default Sidebar;
+export const closeIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+  >
+    <path
+      d="M18 6L6 18M6 6L18 18"
+      stroke="white"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+);
 export const verifyIcon = (
   <svg
     xmlns="http://www.w3.org/2000/svg"
@@ -90,9 +123,9 @@ export const verifyIcon = (
     <path
       d="M9 12L11 14L15.5 9.5M17.9012 4.99851C18.1071 5.49653 18.5024 5.8924 19.0001 6.09907L20.7452 6.82198C21.2433 7.02828 21.639 7.42399 21.8453 7.92206C22.0516 8.42012 22.0516 8.97974 21.8453 9.47781L21.1229 11.2218C20.9165 11.7201 20.9162 12.2803 21.1236 12.7783L21.8447 14.5218C21.9469 14.7685 21.9996 15.0329 21.9996 15.2999C21.9997 15.567 21.9471 15.8314 21.8449 16.0781C21.7427 16.3249 21.5929 16.549 21.4041 16.7378C21.2152 16.9266 20.991 17.0764 20.7443 17.1785L19.0004 17.9009C18.5023 18.1068 18.1065 18.5021 17.8998 18.9998L17.1769 20.745C16.9706 21.2431 16.575 21.6388 16.0769 21.8451C15.5789 22.0514 15.0193 22.0514 14.5212 21.8451L12.7773 21.1227C12.2792 20.9169 11.7198 20.9173 11.2221 21.1239L9.47689 21.8458C8.97912 22.0516 8.42001 22.0514 7.92237 21.8453C7.42473 21.6391 7.02925 21.2439 6.82281 20.7464L6.09972 19.0006C5.8938 18.5026 5.49854 18.1067 5.00085 17.9L3.25566 17.1771C2.75783 16.9709 2.36226 16.5754 2.15588 16.0777C1.94951 15.5799 1.94923 15.0205 2.1551 14.5225L2.87746 12.7786C3.08325 12.2805 3.08283 11.7211 2.8763 11.2233L2.15497 9.47678C2.0527 9.2301 2.00004 8.96568 2 8.69863C1.99996 8.43159 2.05253 8.16715 2.15472 7.92043C2.25691 7.67372 2.40671 7.44955 2.59557 7.26075C2.78442 7.07195 3.00862 6.92222 3.25537 6.8201L4.9993 6.09772C5.49687 5.89197 5.89248 5.4972 6.0993 5.00006L6.82218 3.25481C7.02848 2.75674 7.42418 2.36103 7.92222 2.15473C8.42027 1.94842 8.97987 1.94842 9.47792 2.15473L11.2218 2.87712C11.7199 3.08291 12.2793 3.08249 12.7771 2.87595L14.523 2.15585C15.021 1.94966 15.5804 1.9497 16.0784 2.15597C16.5763 2.36223 16.972 2.75783 17.1783 3.25576L17.9014 5.00153L17.9012 4.99851Z"
       stroke="white"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
     />
   </svg>
 );
@@ -107,9 +140,9 @@ export const barIcon = (
     <path
       d="M18 20V10M12 20V4M6 20V14"
       stroke="#D6BBFB"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
     />
   </svg>
 );
