dcac98a3263693daa875a0317b48bcac9660dbb3
lalit rana <lalit.1995.rana@gmail.com>
2024-08-11 17:10:58 +0530

    changes for the kycmain file for the imageupload and bank details

--- src/components/dashboard/KYC/address.jsx
+++ src/components/dashboard/KYC/address.jsx
@@ -1,6 +1,9 @@
 import React, { useState, useContext, useEffect, useRef } from "react";
 import Navbtn from "../../common/button/navbtn/navBtnTwo";
 import styles from "./address.module.css";
+import { addAddressProof } from "../../../servicefile/kycservice";
+import { ToastContainer, toast } from "react-toastify";
+
 const SelectField = ({ label, placeholder }) => {
   return (
     <div className={styles.SelectInput}>
@@ -21,23 +24,38 @@ const SelectField = ({ label, placeholder }) => {
   );
 };
 
-export const TextField = ({ label, placeholder }) => {
+export const TextField = ({
+  label,
+  placeholder,
+  currentValue,
+  setValue,
+  type,
+}) => {
   return (
     <div className={styles.TextInput}>
       <div className={styles.TextInputWithLabel}>
         <div className={styles.TextLabel}>{label}</div>
         <input
-          type="text"
+          type={type ? type : "text"}
           className={styles.TextFieldInput}
           placeholder={placeholder}
+          value={currentValue}
+          onChange={(e) => setValue(e.target.value)}
         />
       </div>
     </div>
   );
 };
-function AddressDetail() {
+function AddressDetail({ setStepReload }) {
   const imageRef = useRef(null);
   const [file, handleFile] = useState("");
+
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [addressProofType, setAddressProofType] = useState("");
+  const [documentNumber, setDocumentNumber] = useState("");
+  const [proofState, setProofState] = useState("");
+
   const handleClick = (event) => {
     imageRef.current.click();
   };
@@ -46,13 +64,34 @@ function AddressDetail() {
     const fileUploaded = event.target.files[0];
     handleFile(fileUploaded);
   };
-  // const addAddress = async () => {
-  //   const res = await addPanCard(panCardNo);
-  //   if (res && res.message === "PanCard Added!") {
-  //     localStorage.setItem("transactionInfo", "true");
-  //     toast.success(`${res.message}`);
-  //   }
-  // };
+
+  const addAddressData = async () => {
+    if (
+      !file ||
+      !firstName ||
+      !lastName ||
+      !addressProofType ||
+      !documentNumber ||
+      !proofState
+    ) {
+      toast.error(`All fields are required for saving address information!`);
+    } else {
+      const res = await addAddressProof(
+        file,
+        firstName,
+        lastName,
+        addressProofType,
+        documentNumber,
+        proofState
+      );
+      if (res && res.message === "success") {
+        localStorage.setItem("transactionInfo", "true");
+        toast.success(`Address information saved!`);
+        setStepReload(true);
+      }
+    }
+  };
+
   return (
     <>
       <div className={styles.AddressDetailsContainer}>
@@ -60,21 +99,40 @@ function AddressDetail() {
         <div className={styles.AddressDetailsForm}>
           <div className={styles.AddressDetailsContent}>
             <div className={styles.InputRow}>
-              <TextField label="First name" placeholder="Olivia" />
-              <TextField label="Last name" placeholder="Rhye" />
+              <TextField
+                label="First name"
+                placeholder="Olivia"
+                currentValue={firstName}
+                setValue={setFirstName}
+              />
+              <TextField
+                label="Last name"
+                placeholder="Rhye"
+                currentValue={lastName}
+                setValue={setLastName}
+              />
             </div>
             <div className={styles.InputRow}>
-              <SelectField
+              <TextField
                 label="Address Proof Document Type"
                 placeholder="Address Proof"
+                currentValue={addressProofType}
+                setValue={setAddressProofType}
               />
               <TextField
                 label="Address Proof Document Number"
                 placeholder="GSVD73YB3B"
+                currentValue={documentNumber}
+                setValue={setDocumentNumber}
               />
             </div>
             <div className={styles.InputRow}>
-              <SelectField label="State" placeholder="Bihar" />
+              <TextField
+                label="State"
+                placeholder="Bihar"
+                currentValue={proofState}
+                setValue={setProofState}
+              />
             </div>
             <div className={styles.UploadArea}>
               <div className={styles.UploadLabel}>
@@ -109,9 +167,11 @@ function AddressDetail() {
                         <div className={styles.Navbtn}>
                           <div className={styles.Text}></div>
                         </div>
-                        {/* <div className={styles.ActionText}>
-                          or drag and drop
-                        </div> */}
+                        {file && file.name && (
+                          <div className={styles.ActionText}>
+                            Selected File : {file.name}
+                          </div>
+                        )}
                       </div>
                       <div className={styles.ActionSubtext}>
                         JPG or JPEG (Max Size 2MB)
@@ -131,11 +191,15 @@ function AddressDetail() {
                   bg="#3968EB"
                   color="white"
                   showIcon={false}
+                  onClickNav={() => {
+                    addAddressData();
+                  }}
                 />
               </div>
             </div>
           </div>
         </div>
+        <ToastContainer />
       </div>
     </>
   );
--- src/components/dashboard/KYC/bankDetails.jsx
+++ src/components/dashboard/KYC/bankDetails.jsx
@@ -1,8 +1,39 @@
+import React, { useState } from "react";
 import Navbtn from "../../common/button/navbtn/navbtn";
 import styles from "./address.module.css";
 import { TextField } from "./address";
+import { addBankDetails } from "../../../servicefile/kycservice";
+import { ToastContainer, toast } from "react-toastify";
 
-function BankAccDetails() {
+function BankAccDetails({ setStepReload }) {
+  const [account_number, setAccountNumber] = useState("");
+  const [bank_name, setBankName] = useState("");
+  const [ifsc_code, setIfscCode] = useState("");
+  const [validate, setValidate] = useState("");
+
+  const addBankDetailsData = async () => {
+    if (!account_number || !bank_name || !ifsc_code || !validate) {
+      toast.error(`All fields are required for saving address information!`);
+    } else if (account_number !== validate) {
+      toast.error(
+        `Account Number value is not same in Account Number and Verify Account Number fields!`
+      );
+    } else if (
+      account_number &&
+      bank_name &&
+      ifsc_code &&
+      validate &&
+      account_number === validate
+    ) {
+      const res = await addBankDetails(account_number, bank_name, ifsc_code);
+      if (res && res.message === "Bank Saved Successfully!") {
+        localStorage.setItem("transactionInfo", "true");
+        toast.success(`Bank information saved!`);
+        setStepReload(true);
+      }
+    }
+  };
+
   return (
     <>
       <div className={styles.AddressDetailsContainer}>
@@ -13,22 +44,42 @@ function BankAccDetails() {
               <TextField
                 label="Account Number"
                 placeholder="*********************"
+                type="password"
+                value={account_number}
+                setValue={setAccountNumber}
               />
               <TextField
                 label="Verify Account Number"
                 placeholder="83832993803748"
+                value={validate}
+                setValue={setValidate}
               />
             </div>
             <div className={styles.InputRow}>
               {" "}
-              <TextField label="Bank Name" placeholder="ICICI Bank" />
-              <TextField label="IFSC Code" placeholder="ICICI22881" />
+              <TextField
+                label="Bank Name"
+                placeholder="ICICI Bank"
+                value={bank_name}
+                setValue={setBankName}
+              />
+              <TextField
+                label="IFSC Code"
+                placeholder="ICICI22881"
+                value={ifsc_code}
+                setValue={setIfscCode}
+              />
             </div>
           </div>
           <div className={styles.FormFooter}>
             <div className={styles.Divider}></div>
             <div className={styles.Content}>
-              <div className={styles.Actions}>
+              <div
+                className={styles.Actions}
+                onClick={() => {
+                  addBankDetailsData();
+                }}
+              >
                 <Navbtn
                   text="Save changes"
                   bg="#3968EB"
@@ -39,6 +90,7 @@ function BankAccDetails() {
             </div>
           </div>
         </div>
+        <ToastContainer />
       </div>
     </>
   );
