8ca25567ddd290c9175bd162e022686f778b2f4a
Abhinav Sharma <nabu1798@gmail.com>
2025-03-04 16:54:52 +0530

    landing page responsive changes and withraw modal fixed.

--- src/components/home/aboutus/aboutUs.jsx
+++ src/components/home/aboutus/aboutUs.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import styles from "./aboutUs.module.css";
 import aboutusimg from "../../../assets/aboutus.png";
 import Reveal from "../../common/reveal/Reveal";
+import { Card } from "react-bootstrap";
+import { getPokerSiteImage } from "../../../helperFxns/colorCode";
 
 const bulletLists = [
   {
@@ -12,12 +14,42 @@ const bulletLists = [
   },
 ];
 
-const animationTransition = {
-  duration: 0.5,
-  delay: 0.65,
-};
 
 const AboutUs = () => {
+  const sliderRef = useRef(null);
+  const users = [
+    { name: "Junglee Poker" },
+    { name: "MPL" },
+     { name: "Poker Baazi" }, 
+     { name: "A23poker" }, 
+     { name: "PokerCircle" }, 
+     { name: "PokerDangal" },
+      { name: "Natural8" }, 
+      { name: "Pocket52" },
+      { name: "Adda52" },
+       { name: "ACRpoker" },
+        { name: "CoinPoker" },
+       { name: "WPTglobal" }
+  ];
+  const pokerSites = {
+    "Junglee Poker": "https://www.jungleerummy.com/",
+    "MPL": "https://www.mpl.live/",
+    "Poker Baazi": "https://www.pokerbaazi.com/",
+    "A23poker": "https://www.a23.com/",
+    "PokerCircle": "https://www.pokercircle.com/",
+    "PokerDangal": "https://www.pokerdangal.com/",
+    "Natural8": "https://www.natural8.com/",
+    "Pocket52": "https://www.pocket52.com/",
+    "Adda52": "https://www.adda52.com/",
+    "ACRpoker": "https://www.americascardroom.eu/",
+    "CoinPoker": "https://www.coinpoker.com/",
+    "WPTglobal": "https://www.wptglobal.com/"
+  };
+
+  const animationTransition = {
+    duration: 0.5,
+    delay: 0.65,
+  };
   return (
     <div id="about-us" className={`${styles.about__container} `}>
       <div className={styles.content}>
@@ -42,7 +74,7 @@ const AboutUs = () => {
             <div className={styles.head_info}>
               <div className={styles.head_cont}>
                 <div className={styles.head_text}>
-                  What is  <span style={{color:"#ffbf00",fontWeight:"700",fontFamily:"Futura"}}>Rakeback</span> – Get Paid to Play
+                  What is  <span style={{ color: "#ffbf00", fontWeight: "700", fontFamily: "Futura" }}>Rakeback</span> – Get Paid to Play
                 </div>
                 <div className={styles.subhead_text}>
                   When you play online poker, a portion of your bets, known as
@@ -92,6 +124,27 @@ const AboutUs = () => {
           <img src={aboutusimg} width={500} height={420} alt="" />
         </Reveal>
       </div>
+      {/* <div className={styles.slider_container} ref={sliderRef}>
+        <div className={styles.slider}>
+          {[...users, ...users].map((user, index) => (
+            <a
+              key={index}
+              href={pokerSites[user.name] || "#"}
+              target="_blank"
+              rel="noopener noreferrer"
+              className={styles.card_link}
+            >
+              <div className={styles.user_card}>
+                <div className={styles.card_body_custom}>
+                  <div className={styles.logo_container}>
+                    <img src={getPokerSiteImage(user.name)} alt={user?.name} className={styles.logo} />
+                  </div>
+                </div>
+              </div>
+            </a>
+          ))}
+        </div>
+      </div>; */}
     </div>
   );
 };
@@ -118,36 +171,36 @@ const BulletIcon = () => (
   </svg>
 );
 const Arrow = () => (
-	<svg
-	  xmlns="http://www.w3.org/2000/svg"
-	  width="24"
-	  height="24"
-	  viewBox="0 0 24 24"
-	  fill="none"
-	  className="arrow-icon"
-	>
-	  <path
-		d="M5 12H19"
-		stroke="currentColor"
-		strokeWidth="1.5"
-		strokeLinecap="round"
-		strokeLinejoin="round"
-	  />
-	  <path
-		d="M15 16L19 12"
-		stroke="currentColor"
-		strokeWidth="1.5"
-		strokeLinecap="round"
-		strokeLinejoin="round"
-	  />
-	  <path
-		d="M15 8L19 12"
-		stroke="currentColor"
-		strokeWidth="1.5"
-		strokeLinecap="round"
-		strokeLinejoin="round"
-	  />
-	</svg>
-  );
-  
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    className="arrow-icon"
+  >
+    <path
+      d="M5 12H19"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M15 16L19 12"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M15 8L19 12"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
 
