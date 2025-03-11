cfba78b07f5d2be07daef2eb29ce34383dc16552
Abhinav Sharma <nabu1798@gmail.com>
2025-03-11 18:08:51 +0530

    Withdraw icon change

--- src/components/dashboard/home/RakebackChart.jsx
+++ src/components/dashboard/home/RakebackChart.jsx
@@ -34,7 +34,7 @@ ChartJS.register(
 
 const RakebackChart = ({ dashboardInfo, userKyc }) => {
   const { userWallet } = dashboardInfo;
- const navigate = useNavigate();
+  const navigate = useNavigate();
   console.log(dashboardInfo, userWallet, " 25--");
   const [showWithdraw, setShowWithdraw] = useState(false);
 
@@ -101,12 +101,12 @@ const RakebackChart = ({ dashboardInfo, userKyc }) => {
       percentage: [20, 25, 15, 18, 12, 6, 22],
     },
     thisMonth: {
-      earned: [250, 300, 200, 280,430,510],
-      percentage: [20, 25, 15, 18,25,32],
+      earned: [250, 300, 200, 280, 430, 510],
+      percentage: [20, 25, 15, 18, 25, 32],
     },
     thisYear: {
-      earned: [1200, 1500, 1000, 1300,1800,2400],
-      percentage: [20, 25, 15, 18,22,25],
+      earned: [1200, 1500, 1000, 1300, 1800, 2400],
+      percentage: [20, 25, 15, 18, 22, 25],
     },
   };
 
@@ -139,7 +139,7 @@ const RakebackChart = ({ dashboardInfo, userKyc }) => {
         pointRadius: 6,
         pointHoverRadius: 8,
         fill: true,
-        fontSize:"12px",
+        fontSize: "12px",
         tension: 0.4,
       },
     ],
@@ -188,7 +188,7 @@ const RakebackChart = ({ dashboardInfo, userKyc }) => {
       {kycPop && <KycPopup setKycPop={setKycPop} />}
 
       <div
-       className={styles.main_container}
+        className={styles.main_container}
       >
         <div
           style={{
@@ -200,7 +200,8 @@ const RakebackChart = ({ dashboardInfo, userKyc }) => {
         >
           {/* User Info Card */}
           <div className={styles.dashboard_card} style={{
-            backgroundImage: `url(${backgroundImg})` }}>
+            backgroundImage: `url(${backgroundImg})`
+          }}>
             <div>
               <div className={styles.dashboard_greeting}>Hello, {dashboardInfo.user.userName}</div>
               {/* <div className={styles.dashboard_username}>
@@ -211,7 +212,7 @@ const RakebackChart = ({ dashboardInfo, userKyc }) => {
               </div>
               <div className={styles.explore} >
                 <button
-                  onClick={()=>validatetokenAndRedirect()}
+                  onClick={() => validatetokenAndRedirect()}
                 >
                   Explore
                   <Arrow />
@@ -233,11 +234,11 @@ const RakebackChart = ({ dashboardInfo, userKyc }) => {
             {selectedFilter === "thisMonth" && "This Month"}
             {selectedFilter === "thisYear" && "This Year"}
           </div>
-          <div className={styles.chart_container}>          
+          <div className={styles.chart_container}>
             <Line
-            data={chartData}
-            options={chartOptions}
-          />
+              data={chartData}
+              options={chartOptions}
+            />
           </div>
 
         </div>
@@ -277,33 +278,17 @@ const RakebackChart = ({ dashboardInfo, userKyc }) => {
           </div>
 
           <div
-            style={{
-              width: "100%",
-              maxWidth: "300px",
-              borderRadius: "16px",
-              backgroundColor: "#0052cc",
-              height: "250px",
-              padding: "20px",
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              color: "#fff",
-              boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.2)",
-            }}
+
             className={styles.withdrawal_container}
           >
-            <div className={styles.withdrawal_container_inner}></div>
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              width="26"
-              height="24"
               viewBox="0 0 26 26"
               fill="none"
               className={styles.withdraw_icon}
             >
               <path
                 d="M17.1667 8.83358V5.18843C17.1667 4.32204 17.1667 3.88884 16.9842 3.62262C16.8247 3.39002 16.5778 3.23202 16.2997 3.18471C15.9815 3.13056 15.5882 3.31209 14.8016 3.67517L5.56147 7.93982C4.8599 8.26363 4.50912 8.42553 4.25219 8.67662C4.02506 8.8986 3.85168 9.16957 3.74532 9.46882C3.625 9.80732 3.625 10.1937 3.625 10.9664V16.1252M17.6875 15.6044H17.6979M3.625 12.1669L3.625 19.0419C3.625 20.2087 3.625 20.7921 3.85207 21.2377C4.05181 21.6297 4.37052 21.9484 4.76252 22.1482C5.20817 22.3752 5.79156 22.3752 6.95833 22.3752H19.0417C20.2084 22.3752 20.7918 22.3752 21.2375 22.1482C21.6295 21.9484 21.9482 21.6297 22.1479 21.2377C22.375 20.7921 22.375 20.2087 22.375 19.0419V12.1669M17.6875 15.6044H17.6979"
-                stroke="#175CD3"
                 strokeWidth="2"
                 strokeLinecap="round"
                 strokeLinejoin="round"
@@ -313,8 +298,8 @@ const RakebackChart = ({ dashboardInfo, userKyc }) => {
             <div className={styles.wallet_balance_head}>Your Balance</div>
 
             <div
-           
-            className={styles.wallet_balance}
+
+              className={styles.wallet_balance}
             >
               ₹
               {userWallet.wallet_balance
