f5a0a3ba9c7fe1cae4c831cce061b5ca5935a307
mandeep <mandeep@1wayit.com>
2025-08-21 18:01:23 +0530

    Deals PAge HEader

--- src/components/offerAndDeal/header/header.jsx
+++ src/components/offerAndDeal/header/header.jsx
@@ -5,11 +5,17 @@ import Navbtn from "../../common/button/navbtn/navbtn";
 const OfferHeader = ({ setSearchTerm, searchTerm }) => {
 	return (
 		<div className={styles.header_container}>
-			<SectionHeader
+			{/* <SectionHeader
 				head='Hottest Deals'
-				subhead="With Rakebackk, it's not just about playing,  it's about earning more every time you do.   "
+				subhead="   "
 				offerHead={true}
-			/>
+			/> */}
+			<div className="DealsOfferHed">
+				<div class="col-lg-12 text-center">
+					<h1 class="title">Hottest  <span class="titleAccent">Deals</span></h1>
+					<p class="subtitle">With Rakebackk, it's not just about playing,  it's about earning more every time you do.</p>
+				</div>				
+			</div>
 			{/* <div className={styles.email_input_container}>
 				<div className={styles.input_content}>
 					<input
